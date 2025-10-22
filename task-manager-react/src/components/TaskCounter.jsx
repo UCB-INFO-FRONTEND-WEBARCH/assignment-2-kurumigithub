690feae7ad501c398a382af817fdae12c67a3f15
Kurumi <kskaneko@ucsd.edu>
2025-10-22 14:59:49 -0700

    change checkmark icon image

--- task-manager-react/src/components/TaskCounter.jsx
+++ task-manager-react/src/components/TaskCounter.jsx
@@ -1,10 +1,12 @@
+import checkmarkIcon from './../assets/check_icon.png'
+
 function TaskCounter({ tasks }) {
   const totalTasks = tasks.length;
   const completedTasks = tasks.filter(task => task.completed).length;
 
     return ( 
         <div className="main-header__status">
-            <img src="../../src/assets/check_icon.png" alt="check-mark" className="check-icon" />
+            <img src={checkmarkIcon} alt="check-mark" className="check-icon" />
             <span>{totalTasks} / {completedTasks}</span>
         </div>
     );
